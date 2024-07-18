1221501cb14561cc6173780402cafa0c3e7ced50
Edward Bittner <6522402+edhead990@users.noreply.github.com>
2024-07-19 04:42:18 +0930

    Updated poop scene and added sprites.

--- Poop.gd
+++ Poop.gd
@@ -1,17 +1,17 @@
-extends ColorRect
+extends Button
 
 var spawn_position: Vector2
 @onready var clean_timer = $CleanTimer
 
 func _ready():
-	global_position = spawn_position
-
-func _on_clean_button_button_down():
-	clean_timer.start()
-
-func _on_clean_button_button_up():
-	clean_timer.stop()
+	global_position = spawn_position - Vector2(0.0, 10.0)
 
 func _on_clean_timer_timeout():
 	get_parent().poop_counter -= 1
 	queue_free()
+
+func _on_button_up():
+	clean_timer.stop()
+
+func _on_button_down():
+	clean_timer.start()
